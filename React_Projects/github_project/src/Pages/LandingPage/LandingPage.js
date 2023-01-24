e61c3d3777a0dc0ac6d7bff82f1d881a44799c3b
Tommy Nguyen <tommynguyen@TommyNguyensMBP.attlocal.net>
2023-01-24 14:37:46 -0600

    Add button

--- React_Projects/github_project/src/Pages/LandingPage/LandingPage.js
+++ React_Projects/github_project/src/Pages/LandingPage/LandingPage.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import ListView from './components/ContributerListView/ContributerListView';
 import ContributerInfoDrawer from './components/ContributerInfoDrawer';
 import { ContributerList } from '../../Shared/Context/GithubProvider';
+import { NextPage, PreviousPage } from '../../Shared/Context/GithubProvider';
+import { Button, Grid } from '@mui/material';
 import './LandingPage.css'
 
 
 const LandingPage = () => {
     const listOfContributers = ContributerList();
+    const navigateNextPage = NextPage();
+    const navigatePreviousPage = PreviousPage();
     const [openDrawer, setOpenDrawer] = useState(false);
     return (
         <div className='background-color'>
@@ -15,6 +19,14 @@ const LandingPage = () => {
                 const {login, contributions, avatar_url} = contributer;
                 return <ListView username={login} contributionAmount={contributions} avatar={avatar_url} openDrawer={() => {setOpenDrawer(true);}} />
             }) : <></>}
+            <Grid container spacing={4} className='content'>
+                <Grid item>
+                    <Button variant='contained' onClick={() => {navigatePreviousPage()}}> Previous </Button>
+                </Grid>
+                <Grid item>
+                    <Button variant='contained' onClick={() => {navigateNextPage()}}> Next </Button>
+                </Grid>
+            </Grid>
             <ContributerInfoDrawer isOpen={openDrawer} setOpen={setOpenDrawer}/>
         </div>
     )
